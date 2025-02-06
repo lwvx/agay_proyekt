db385986e4c15540bf07dafc769ad732770b2fd9
Kadirov Muhammadaziz <126769438+KadirovKmck@users.noreply.github.com>
2025-02-06 12:06:35 +0600

    its done

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
+import 'package:agay_proyekt/src/presentation/splash_screen/splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
-import 'src/presentation/rental_expenses/rental_expenses.dart';
-import 'src/presentation/splash_screen/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +14,7 @@ class MyApp extends StatelessWidget {
     return ResponsiveSizer(
       builder: (p0, p1, p2) => MaterialApp(
         debugShowCheckedModeBanner: false,
-        home: const RentalExpenses(),
+        home: const SplashScreen(),
       ),
     );
   }
---
+++ lib/src/presentation/home/widget/every_month_widget.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_sizer/responsive_sizer.dart';
+
+class EverymonthWidget extends StatelessWidget {
+  final String? highlightedProperty;
+  final Function(String) onSelect;
+
+  const EverymonthWidget({
+    super.key,
+    required this.highlightedProperty,
+    required this.onSelect,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final List<Map<String, dynamic>> properties = [
+      {'label': 'Every day'},
+      {'label': 'Every week'},
+      {'label': 'Every 2 weeks'},
+      {'label': 'Every 3 weeks'},
+      {'label': 'Every month'},
+    ];
+
+    return Container(
+      height: 70.h,
+      padding: EdgeInsets.all(16.sp),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              SizedBox(width: 6.w),
+              Text(
+                'Select the type of real estate',
+                style: TextStyle(
+                  fontSize: 18.sp,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+              GestureDetector(
+                onTap: () => Navigator.pop(context),
+                child: Container(
+                  height: 2.5.h,
+                  width: 5.5.w,
+                  decoration: BoxDecoration(
+                    color: const Color(0xff393937),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Icon(
+                    Icons.close,
+                    color: Colors.white,
+                    size: 18.sp,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 2.h),
+          Expanded(
+            child: GridView.builder(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                childAspectRatio: 3,
+                crossAxisSpacing: 8,
+                mainAxisSpacing: 8,
+              ),
+              itemCount: properties.length,
+              itemBuilder: (context, index) {
+                bool isSelected =
+                    properties[index]['label'] == highlightedProperty;
+                return GestureDetector(
+                  onTap: () {
+                    onSelect(properties[index]['label']);
+                    Navigator.pop(context);
+                  },
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: isSelected
+                          ? const Color(0xff325FD3)
+                          : const Color(0xff393937),
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    child: Center(
+                      child: Text(
+                        properties[index]['label'],
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 15.sp,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
